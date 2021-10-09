992ef925c7284c7b0f8319f83e4c31d73a1eaddd
Jake Mulley <jakemulley@users.noreply.github.com>
2021-10-09 20:57:03 +0100

    Only minify CSS on production build

--- src/transforms/css.js
+++ src/transforms/css.js
@@ -1,14 +1,19 @@
-const postcss = require('postcss')([
+const plugins = [
   require('postcss-import'),
   require('precss'),
   require('autoprefixer'),
   require('@fullhuman/postcss-purgecss')({
     content: ['./docs/**/*.html']
-  }),
-  require('cssnano')({
-    preset: 'default'
   })
-])
+]
+
+if (process.env.ELEVENTY_ENV === 'production') {
+  plugins.push(require('cssnano')({
+    preset: 'default'
+  }))
+}
+
+const postcss = require('postcss')(plugins)
 
 module.exports = async (content, outputPath) => {
   if (outputPath && outputPath.endsWith('.css')) {
