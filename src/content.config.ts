e90dac4bc6f80aabd5e501b2774ee8db173cb864
jakemulley <jakemulley@users.noreply.github.com>
2026-01-01 19:21:59 +0000

    chore: upgrade astro collections to v2 API

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date()
+  })
+});
+
+export const collections = { blog };
