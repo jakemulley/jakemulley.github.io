63f6f433afa164c403cf0659282280ca154346af
jakemulley <jakemulley@users.noreply.github.com>
2026-01-01 18:24:43 +0000

    chore: upgrade to tailwind@v4.1.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
